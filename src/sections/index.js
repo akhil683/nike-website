11d8ac9c974db9660b9ccf6bb23806883879dbd7
akhil683 <onkar.palsra@gmail.com>
2023-08-23 01:15:10 +0530

    inital commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services'
+import Footer from './footer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe'
+import SpecialOffer from './SpecialOffer';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    Footer,
+    CustomerReviews,
+    Subscribe,
+    SpecialOffer
+} 
